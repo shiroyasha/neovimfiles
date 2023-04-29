235e0f8fb9a9002f4a564dc20c21f5d4e13113d6
Igor Šarčević <igor@renderedtext.com>
2023-04-29 18:25:18 +0000

    Add plugins for elixir

--- lua/shiroyasha/plugins.lua
+++ lua/shiroyasha/plugins.lua
@@ -17,36 +17,19 @@ Plug('nvim-lualine/lualine.nvim')
 
 Plug("nvim-telescope/telescope.nvim", {branch = "0.1.x" })
 
--- Plug 'Shougo/unite.vim'
--- Plug 'Shougo/vimfiler.vim'
--- Plug 'AndrewRadev/splitjoin.vim'
--- Plug 'danchoi/ri.vim'
--- Plug 'danro/rename.vim'
--- Plug 'godlygeek/tabular'
--- Plug 'isRuslan/vim-es6'
--- Plug 'shiroyasha/make-test'
--- Plug 'kana/vim-textobj-user'
--- Plug 'kien/ctrlp.vim'
--- Plug 'rking/ag.vim'
--- Plug 'nelstrom/vim-textobj-rubyblock'
--- Plug 'neomake/neomake'
--- Plug 'rainerborene/vim-reek'
--- Plug 'sheerun/vim-polyglot'
--- Plug 'tpope/vim-endwise'
--- Plug 'tpope/vim-fugitive'
--- Plug 'tpope/vim-rails'
--- Plug 'tpope/vim-repeat'
--- Plug 'tpope/vim-surround'
--- Plug 'vim-airline/vim-airline'
--- Plug 'thisivan/vim-bufexplorer'
--- Plug 'tpope/vim-dispatch'
--- Plug 'elixir-editors/vim-elixir'
--- Plug 'renderedtext/vim-elixir-alternativ-files'
--- Plug 'fatih/vim-go'
--- Plug 'shiroyasha/make-test'
--- Plug 'mhinz/vim-mix-format'
--- Plug 'github/copilot.vim'
--- Plug 'neovim/nvim-lspconfig'
+-- autocompletion
+Plug("hrsh7th/nvim-cmp")   -- completion plugin
+Plug("hrsh7th/cmp-buffer") -- source for text in buffer
+Plug("hrsh7th/cmp-path")   -- source for file system paths
+
+Plug('shiroyasha/make-test')
+
+Plug('elixir-editors/vim-elixir')
+Plug('fatih/vim-go')
+Plug('mhinz/vim-mix-format')
+Plug('github/copilot.vim')
+Plug('neovim/nvim-lspconfig')
+
 -- Plug 'hrsh7th/cmp-nvim-lsp', { 'branch' = 'main' }
 -- Plug 'hrsh7th/cmp-buffer', { 'branch' = 'main' }
 -- Plug 'hrsh7th/cmp-path', { 'branch' = 'main' }
