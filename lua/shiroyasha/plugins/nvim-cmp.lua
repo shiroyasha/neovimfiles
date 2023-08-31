8ca5e0dc987775c63e7f198df1f27648cec8af5b
Igor Šarčević <igor@renderedtext.com>
2023-08-31 10:47:45 +0000

    Fix autocomplete menu

--- lua/shiroyasha/plugins/nvim-cmp.lua
+++ lua/shiroyasha/plugins/nvim-cmp.lua
@@ -1,20 +1,5 @@
--- import nvim-cmp plugin safely
-local cmp_status, cmp = pcall(require, "cmp")
-if not cmp_status then
-  return
-end
-
--- import luasnip plugin safely
-local luasnip_status, luasnip = pcall(require, "luasnip")
-if not luasnip_status then
-  return
-end
-
--- import lspkind plugin safely
-local lspkind_status, lspkind = pcall(require, "lspkind")
-if not lspkind_status then
-  return
-end
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require("luasnip/loaders/from_vscode").lazy_load()
@@ -43,11 +28,4 @@ cmp.setup({
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
   }),
-  -- configure lspkind for vs-code like icons
-  formatting = {
-    format = lspkind.cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
 })
