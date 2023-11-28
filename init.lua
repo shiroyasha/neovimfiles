5098a6a4137ce1764c7454878fe542da4bbcf8b6
Igor Šarčević <igor@renderedtext.com>
2023-11-28 16:33:10 +0000

    Add shortcut for opening code actions

--- init.lua
+++ init.lua
@@ -16,3 +16,10 @@ require("shiroyasha.plugins.treesitter")
 require("shiroyasha.plugins.gitsigns")
 
 require("shiroyasha.core.keymaps")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts)
+  end,
+})
