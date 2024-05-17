54eaabe7130b88df66877f3eab27141c270df051
Igor Šarčević <igor@operately.com>
2024-05-17 20:38:37 +0000

    Disable elixir ls, use only next ls
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lua/shiroyasha/plugins/lsp/elixir.lua
+++ lua/shiroyasha/plugins/lsp/elixir.lua
@@ -4,18 +4,19 @@ local elixirls = require("elixir.elixirls")
 elixir.setup {
   nextls = {
     enable = true,
-  },
-  elixirls = {
-    enable = true,
-    settings = elixirls.settings {
-      dialyzerEnabled = false,
-      enableTestLenses = false,
-    },
+
     on_attach = function(client, bufnr)
       vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
       vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
       vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
       vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
     end,
-  }
+  },
+  -- elixirls = {
+  --   enable = true,
+  --   settings = elixirls.settings {
+  --     dialyzerEnabled = false,
+  --     enableTestLenses = false,
+  --   },
+  -- }
 }
