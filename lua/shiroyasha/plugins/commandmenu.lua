387aa1cf7991e2a33b8bd3a17dc6b31685c1407f
Igor Šarčević <igor@operately.com>
2024-06-20 12:41:37 +0000

    Add new code actions
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lua/shiroyasha/plugins/commandmenu.lua
+++ lua/shiroyasha/plugins/commandmenu.lua
@@ -5,9 +5,11 @@ local finders = require('telescope.finders')
 local conf = require('telescope.config').values
 
 local my_custom_commands = {
-  { description = "TS: Organize Imports", command = function() vim.cmd("TSToolsOrganizeImports") end },
-  { description = "TS: Remove Unused", command = function() vim.cmd("TSToolsRemoveUnused") end },
-  { description = "TS: Add Missing Imports", command = function() vim.cmd("TSToolsAddMissingImports") end },
+  { description = "TS only: Organize Imports", command = function() vim.cmd("TSToolsOrganizeImports") end },
+  { description = "TS only: Remove Unused", command = function() vim.cmd("TSToolsRemoveUnused") end },
+  { description = "TS only: Add Missing Imports", command = function() vim.cmd("TSToolsAddMissingImports") end },
+  { description = "Find references (gr)", command = function() vim.lsp.buf.references() end },
+  { description = "Type Definition (gt)", command = function() vim.lsp.buf.type_definition() end },
 }
 
 function command_picker()
