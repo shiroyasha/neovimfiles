3546650a6a3dc9f2d7c85f8fa1a07bb148425571
Igor Šarčević <igor@operately.com>
2024-10-29 15:32:34 +0000

    Gen steps from step calls
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- init.lua
+++ init.lua
@@ -39,3 +39,33 @@ vim.api.nvim_create_user_command('LoadPathsFromTxt', function()
   vim.fn.setqflist(quickfix_list)
   vim.cmd('copen')
 end, {})
+
+vim.api.nvim_create_user_command("GenSteps", function()
+  -- Get the range of the visual selection
+  local line_start, line_end = vim.fn.line("'<"), vim.fn.line("'>")
+
+  -- Placeholder for the result
+  local new_lines = {}
+
+  -- Loop through the selected lines
+  for line = line_start, line_end do
+    local content = vim.fn.getline(line)
+
+    -- Match and capture the function name after `Steps.`
+    local function_name = content:match("Steps%.([%w_]+)")
+
+    if function_name then
+      -- Create the new line format
+      table.insert(new_lines, "  step :" .. function_name .. ", ctx do")
+      table.insert(new_lines, "    ctx |> UI.click(testid: \"something\")")
+      table.insert(new_lines, "  end")
+      table.insert(new_lines, "") -- For a line break
+    end
+  end
+  --
+  -- Insert the new lines below the selection
+  vim.fn.append(line_end, new_lines)
+
+  -- Delete the original selected lines
+  vim.api.nvim_buf_set_lines(0, line_start - 1, line_end, false, {})
+end, { range = true })
